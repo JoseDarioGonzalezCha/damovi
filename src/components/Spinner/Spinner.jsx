b7fcc68e8962d71d086f1301b1b82245949662a4
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2023-09-22 09:51:26 -0300

    Feat: Create and styles component Spinner

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Loader, SpinnerContainer } from "./Spinner.Style";
+
+export const Spinner = () => {
+  return (
+    <SpinnerContainer>
+      <Loader></Loader>
+    </SpinnerContainer>
+  );
+};
