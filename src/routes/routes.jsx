fca601e3b1da63622b28ef83dd5b0919883555d0
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2023-09-22 09:16:00 -0300

    fix: add past Home

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App";
 
-import { Home } from "../pages/Home";
+import { Home } from "../pages/Home/Home";
 import { About } from "../pages/About";
 import { Contact } from "../pages/Contact";
 
