6c31929d5e8139cf4fa6addf9b55720b1c199cb9
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2023-09-22 09:08:33 -0300

    fix: add Oulet from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-//Contexts
-import { useContext } from "react";
-import { GameListContext } from "./contexts/GameListContext";
-
-//COmponents
-import { GameCard } from "./components/GameCard/GameCard";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const { gameList } = useContext(GameListContext);
-
   return (
     <>
-      {gameList &&
-        gameList.map((movie) => (
-          <GameCard
-            thumbnail={movie.thumbnail}
-            title={movie.title}
-            genre={movie.genre}
-            publisher={movie.publisher}
-            developer={movie.developer}
-          />
-        ))}
+      <Outlet />
     </>
   );
 }
