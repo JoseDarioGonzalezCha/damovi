219d024e2fedf87ac3f3f035c08017285736ad30
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2023-09-22 09:09:18 -0300

    fix: add RouterProvider from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
 //Contexts
 
 import GameListProvider from "./contexts/GameListContext.jsx";
+import { RouterProvider } from "react-router-dom";
+import { route } from "./routes/routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GameListProvider>
-      <App />
+      <RouterProvider router={route} />
     </GameListProvider>
   </React.StrictMode>
 );
